934fc04a709ad282eb77b70643efc27c1859b3f3
Kresimir Jelic <kresimir.jelic@microblink.com>
2023-06-28 14:42:54 +0200

    Added crossplat build: arm64 & amd64

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
-docker build . -t jetiz/rabbitmq:3.12.0
-docker push jetiz/rabbitmq:3.12.0
-
+# docker build . -t jetiz/rabbitmq:3.12.0
+# docker push jetiz/rabbitmq:3.12.0
 
 # TEST 
 #docker run -it --rm -v $PWD/init:/opt/init --entrypoint bash jetiz/rabbitmq:local
-#docker run -it --rm -v $PWD/init:/opt/init jetizrabbitmq:local
+#docker run -it --rm -v $PWD/init:/opt/init jetiz/rabbitmq:local
+
+
+docker buildx build -f Dockerfile --push --platform "linux/arm64,linux/amd64" --tag jetiz/rabbitmq:3.12.0 .
